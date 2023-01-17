b06224129d1cd8646c112104cd951851bc9ea445
mardelys <mardelys1996@gmail.com>
2023-01-17 02:09:20 -0500

    ajust

--- src/Components/Projects/CardProject.jsx
+++ src/Components/Projects/CardProject.jsx
@@ -4,8 +4,6 @@ import { TbBrandJavascript } from 'react-icons/tb'
 import { FiExternalLink } from 'react-icons/fi'
 import './Projects.css'
 
-/*---------Apartir de aqui pego la seccion-----*/
-
 
 
 
@@ -43,37 +41,3 @@ export const ProjectsList = ({ projects }) => {
 };
 export default ProjectsList;
 
-
-
-
-/**export  const Projects =() =>{
-   return (
-      <div className="card-project">
-         
-         <div className="card-body">
-            <div className="card-header">
-               <h1>skdnksadn</h1>
-               <img className="img-project" src={require(`./images/img-ecommerce.png`)} alt="" />
-            </div>
-            <div className="icon-tecnology" >
-               <ul>
-                  <li><FaHtml5 className="fab" /></li>
-                  <li><FaCss3Alt className="fab" /></li>
-                  <li><TbBrandJavascript className="fab"/></li>
-                  <li ><FaReact className="fab"/></li>
-                  
-               </ul>
-            </div>
-            <div className="container-info">
-               <div className="info"><p>jszshfjkashfkjaskf</p></div>
-               <div className="icons">
-                  <a href="#"><FiExternalLink className="icon-external" /></a>
-                  <a href="#"><FaGithub className="icon-external" /></a>
-               </div>
-            </div>
-         </div>
-         
-      </div>
-   )
-}
-export default Projects;*/
--- src/Components/Projects/ContainerProjects.jsx
+++ src/Components/Projects/ContainerProjects.jsx
@@ -1,17 +1,13 @@
-/*Estos son las dependencias que tiene con el otro comentario*/
+
 import React from "react";
 import "./Projects.css";
 import { IoRemoveOutline } from 'react-icons/io5'
-
-/*--------------AQUI INTENTO DE FILTROS-----dependencias ------*/
-
-
 import { useState } from 'react';
 import { ButtonList } from "./ButtonList";
 import { ProjectsList } from "./CardProject";
 import data from './data';
 
-/*Aqui filtro y componente nuevo*/
+
 function RenderProjects() {
    const allCategories = [
       'All',
@@ -69,31 +65,5 @@ export default RenderProjects;
 
 
 
-/*
-
---------SOLO SERIA BORRAR LO QUE INDICO SI SALE MAL otra vez y volver a todo a la normalidad----
-export default function ContainerProjects() {
-  return (
-    <div className="section-projects">
-      <div>
-        
-        <div>
-          <button className="button-filter" on>All</button>
-          <button className="button-filter">My Work</button>
-          <button className="button-filter">Collaborations</button>
-        </div>
-        <div className="container-projects">
-          <Projects />
-          <Projects />
-          <Projects />
-          <Projects />
-          <Projects />
-          <Projects />
-          <Projects />
-        </div>
-      </div>
-    </div>
-  );
-}*/
 
 
